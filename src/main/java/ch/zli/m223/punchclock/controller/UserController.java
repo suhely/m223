1ba0c674b08f8fe0558a91dfb4194750b432ab15
sdu <suhely.durgauhee@youngsolutions.ch>
2020-09-14 16:16:04 +0200

    added javadoc to usercontroller

--- src/main/java/ch/zli/m223/punchclock/controller/UserController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/UserController.java
@@ -1,5 +1,10 @@
 package ch.zli.m223.punchclock.controller;
 
+/**
+ *
+ * The User controller handles the sign up
+ */
+
 import ch.zli.m223.punchclock.domain.ApplicationUser;
 import ch.zli.m223.punchclock.repository.ApplicationUserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,12 +17,23 @@ public class UserController {
     private ApplicationUserRepository applicationUserRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    /**
+     * The Constructor uses the following parameters:
+     * @param applicationUserRepository
+     * @param bCryptPasswordEncoder
+     */
     public UserController(ApplicationUserRepository applicationUserRepository,
                           BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.applicationUserRepository = applicationUserRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    /**
+     * The method "postSignUp" encodes the password the user has set.
+     * It also saves it through the application user repository.
+     * It uses the following parameter:
+     * @param user
+     */
     @PostMapping("/sign-up")
     public void postSignUp(@RequestBody ApplicationUser user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
